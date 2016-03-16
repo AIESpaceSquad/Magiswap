9fdef593634161ff1d38c2debc233b85feb5aa94
Quinn Mosher <quinn.mosher@sea.aie.edu>
2016-03-16 16:03:20 -0700

    Color Interaction & ColorManager Class

---
+++ Magiswap/Assets/Scripts/ColorManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //grab mycolor and cash in ennum?
+	}
+
+    public enum CollisionColor
+    {
+        cc_StaticWhite = 8,
+        cc_ActiveWhite = 9,
+        cc_Red = 10,
+        cc_Blue = 11,
+        cc_yellow = 12,
+        cc_green = 13
+    }
+
+    public static void ChangeColor(GameObject in_target, CollisionColor in_newLayer)
+    {
+        in_target.layer = (int)in_newLayer;
+    }
+
+    public static bool IsColor(GameObject in_subject, CollisionColor in_compareColor)
+    {
+        return (in_subject.layer == (int)in_compareColor);
+    }
+
+    public static bool SameColor(GameObject in_right, GameObject in_left)
+    {
+        return (in_left.layer == in_right.layer);
+    }
+
+}
---
+++ Magiswap/Assets/Scripts/ColorTester.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorTester : MonoBehaviour {
+
+    [SerializeField]
+    GameObject subject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if( Input.GetKeyDown(KeyCode.Q))
+        {
+            ColorManager.ChangeColor(subject, ColorManager.CollisionColor.cc_StaticWhite);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ColorManager.ChangeColor(subject, ColorManager.CollisionColor.cc_ActiveWhite);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ColorManager.ChangeColor(subject, ColorManager.CollisionColor.cc_Red);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ColorManager.ChangeColor(subject, ColorManager.CollisionColor.cc_Blue);
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ColorManager.ChangeColor(subject, ColorManager.CollisionColor.cc_yellow);
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            ColorManager.ChangeColor(subject, ColorManager.CollisionColor.cc_green);
+        }
+    }
+}
