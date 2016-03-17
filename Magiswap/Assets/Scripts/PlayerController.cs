479e5c5cd5d37a39caf5a5ddb514d10a20ef5681
Adrian Caple <adrianocaple@gmail.com>
2016-03-17 16:19:58 -0700

    got LAN network and player controller established

---
+++ Magiswap/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rigidBody;
+    Vector2 jump;
+    bool isGrounded;
+    public int jumpForce;
+    public int movementForce;
+
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+    KeyCode spacebar = KeyCode.Space;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        jump = new Vector3(0, jumpForce);
+        isGrounded = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //isGrounded = true;
+	}
+
+    void OnCollisionEnter2D(Collision2D collide)
+    {
+        if (collide.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+
+    public float PlayerInput()
+    {
+        OnUpLadder();
+        OnJump();
+        return OnMoveLeftRight();
+    }
+
+    float OnMoveLeftRight()
+    {
+        if (Input.GetKey(left))
+        {
+            return -movementForce * Time.deltaTime;
+        }
+        else if (Input.GetKey(right))
+        {
+            return movementForce * Time.deltaTime;
+        }
+        return 0;
+    }
+
+    void OnJump()
+    {
+        if (Input.GetKeyDown(spacebar) && isGrounded)
+        {
+            rigidBody.AddForce(jump);
+            isGrounded = false;
+        }
+    }
+
+    void OnUpLadder()
+    {
+        //for ladders?
+        if (Input.GetKey(up))
+        {
+
+        }
+    }
+}
+
+
+
---
+++ Magiswap/Assets/Scripts/PlayerOneNetworkUpdate.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+// make required component here the component that
+//will handle the physics of the player
+//[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(NetworkIdentity))]
+[RequireComponent(typeof(PlayerController))]
+public class PlayerOneNetworkUpdate : NetworkBehaviour
+{
+    NetworkIdentity _identity;
+    PlayerController _controller;
+    //declare physics component here
+    //Rigidbody2D _rigid;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        _identity = GetComponent<NetworkIdentity>();
+        _controller = GetComponent<PlayerController>();
+        //init physics
+        //_rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(!_identity.isLocalPlayer)
+        {
+            //if player isnt controlled by client
+            return;
+        }
+        else //input goes here
+        {
+            transform.position += new Vector3(_controller.PlayerInput(),0,0); 
+        }
+	}
+}
