1b39fd2795b9de3d7699fee1c10ea8a617e178e9
Adrian Caple <adrianocaple@gmail.com>
2016-03-29 18:05:28 -0700

    character selection and controller manager under construction

---
+++ Magiswap/Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelector : MonoBehaviour
+{
+    public ControllerManager cManager;
+    public GameObject joystick;
+    GameObject jStick1;
+    GameObject jStick2;
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Number of controllers in manager: " + cManager.controllerList.Count);
+        for (int i = 0; i < cManager.controllerList.Count; i++)
+        {
+            jStick1 = Instantiate(joystick, transform.position, Quaternion.identity) as GameObject;
+            jStick1.transform.localScale *= 30;
+            jStick1.transform.parent = transform;
+            //Debug.Log("Created a jstick obj");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Debug.Log(cManager.controllerList[0].controllerName + "_rStickX");
+        if(Input.GetAxis(cManager.controllerList[0].controllerName + "rStickX") >= 1 ||
+           Input.GetAxis(cManager.controllerList[0].controllerName + "rStickX") <= 1)
+        {
+            jStick1.transform.position += new Vector3(Input.GetAxis(cManager.controllerList[0].controllerName + "rStickX"),0 , 0);
+            
+        }
+
+	}
+}
---
+++ Magiswap/Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Controller
+{
+    public string controllerType;
+    public string controllerName;
+    public Controller(string tContollerType, int index)
+    {
+        controllerType = tContollerType;
+        if(controllerType == "Controller (XBOX 360 For Windows)")
+        {
+            controllerName = "gp" + (index + 1) + "_";
+            Debug.Log(controllerName);
+        }
+
+
+    }
+}
+
+
+public class ControllerManager : MonoBehaviour
+{
+    public List<Controller> controllerList = new List<Controller>();
+    public int numberOfControllers;
+	// Use this for initialization
+	void Start ()
+    {
+        string[] _tControllerType = Input.GetJoystickNames();
+       
+        for (int i = 0; i < _tControllerType.Length; i++)
+        {
+            controllerList.Add(new Controller(_tControllerType[i], i));
+            Debug.Log("Controllers Name: " + _tControllerType[i]);
+            numberOfControllers++;
+        }
+        Debug.Log("Number of controllers in manager: " + controllerList.Count);
+        //numberOfControllers = _tControllerType.Length;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
